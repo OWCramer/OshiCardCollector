071c17190d4dd33414a0a621ee95cc3359a6fb23
LarveyOfficial <luisvervaet@gmail.com>
2026-04-26 17:22:23 -0400

    Reapply "Refine segmentation and remove debug logs"
    This reverts commit 303826f5b2c46e52df6a6b3003aedcb567cb4fa4.

--- next.config.ts
+++ next.config.ts
@@ -19,22 +19,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  headers: async () => [
-    {
-      // Apply these headers to all routes in your application
-      source: "/(.*)",
-      headers: [
-        {
-          key: "Cross-Origin-Opener-Policy",
-          value: "same-origin",
-        },
-        {
-          key: "Cross-Origin-Embedder-Policy",
-          value: "require-corp",
-        },
-      ],
-    },
-  ],
 };
 
 export default nextConfig;
